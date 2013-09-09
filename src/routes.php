1cb4ee4d803321198ddcbde47b198e3d9a84e49f
Johnny Green <johnnygreen@gmail.com>
2013-09-09 17:43:56 -0400

    small changes to namespace of controller and config

--- src/routes.php
+++ src/routes.php
@@ -3,12 +3,12 @@
 use Illuminate\Routing\Router;
 
 \Route::filter(
-	\Config::get('johnnygreen/laravel-api::filter.name'),
+	\Config::get('laravel-api::filter.name'),
 	'\Johnnygreen\LaravelApi\Auth\Filter'
 );
 
 \Route::resource(
-	\Config::get('johnnygreen/laravel-api::routes.tokens'),
+	\Config::get('laravel-api::routes.tokens'),
 	'\Johnnygreen\LaravelApi\TokensController',
 	['only' => ['store']]
 );
