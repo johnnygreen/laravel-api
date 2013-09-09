5939109f85648419f7c510d370ce1a98027d6a5d
Johnny Green <johnnygreen@gmail.com>
2013-09-09 17:00:37 -0400

    routes not working for some reason

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-\Route::filter(
-	\Config::get('johnnygreen/laravel-api::filter.name'),
+Route::filter(
+	Config::get('johnnygreen/laravel-api::filter.name'),
 	'\Johnnygreen\LaravelApi\Auth\Filter'
 );
 
-\Route::resource(
-	\Config::get('johnnygreen/laravel-api::routes.tokens'),
+Route::resource(
+	Config::get('johnnygreen/laravel-api::routes.tokens'),
 	'\Johnnygreen\LaravelApi\Controllers\TokensController',
 	['only' => ['store']]
 );
