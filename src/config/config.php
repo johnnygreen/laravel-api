a1ab6098db49773578a2d2d6aece475966c0a26b
Johnny Green <johnnygreen@gmail.com>
2013-11-23 20:35:22 -0500

    fixes spelling mistake in config

--- src/config/config.php
+++ src/config/config.php
@@ -5,7 +5,7 @@ return array(
 	'filter_name'         => 'LaravelApi.auth',
 	'tokens_route'        => 'tokens',
 	'register_handlers'   => true,
-	'register_extentions' => true,
+	'register_extensions' => true,
 	'register_commands'   => true
 
 );
